e4427bee29f552674e061cfcb956171ac4957f03
kafu77 <kafubere99@gmail.com>
2024-09-15 11:48:21 -0700

    logging and exceptions handling

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.CNNClassifier.logging import logger
+
+# Log some messages
+logger.info("Welcome to custom log")
+logger.warning('This is a warning message')
+logger.error('This is an error message')
+logger.debug('This is a debug message')
